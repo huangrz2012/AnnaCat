f5d11f5e28b07aff631eb01e4986d90ac03c2913
huangrongzhao <huangrongzhao@qiyuexinxi.cn>
2019-04-17 13:58:31 +0800

    还只是一个悬浮按钮的Anna(判断Anna是否已经存在与打开关闭悬浮窗权限冲突)

--- app/src/main/java/com/annaCat/MainActivity.java
+++ app/src/main/java/com/annaCat/MainActivity.java
@@ -1,14 +1,26 @@
 package com.annaCat;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+    private WindowManager.LayoutParams layoutParams;
+    private WindowManager windowManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +33,99 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (layoutParams == null){
+                    requestOverlayPermission();
+                }
             }
         });
+    }
+
+    private static final int REQUEST_OVERLAY = 111;
+    private void requestOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!Settings.canDrawOverlays(MainActivity.this)) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+                alertDialog.setMessage("是否前往获取悬浮窗权限");
+                alertDialog.setPositiveButton("确定",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                        Uri.parse("package:" + getPackageName()));
+                                startActivityForResult(intent, REQUEST_OVERLAY);
+                            }
+                        });
+                alertDialog.setNegativeButton("取消",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            } else {
+                initAnna();
+            }
+        }else {
+            initAnna();
+        }
+    }
+
+    public void initAnna(){
+
+        // 获取WindowManager服务
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        // 新建悬浮窗控件
+        Button button = new Button(getApplicationContext());
+        button.setText("Anna");
+        button.setBackgroundColor(Color.BLUE);
+        button.setOnTouchListener(new FloatingOnTouchListener());
+
+        // 设置LayoutParam
+        layoutParams = new WindowManager.LayoutParams();
+        layoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+
+        layoutParams.format = PixelFormat.RGBA_8888;
+        layoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+
+        // 将悬浮窗控件添加到WindowManager
+        windowManager.addView(button, layoutParams);
+
+    }
+
+    private class FloatingOnTouchListener implements View.OnTouchListener {
+        private int x;
+        private int y;
+
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    x = (int) event.getRawX();
+                    y = (int) event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    int nowX = (int) event.getRawX();
+                    int nowY = (int) event.getRawY();
+                    int movedX = nowX - x;
+                    int movedY = nowY - y;
+                    x = nowX;
+                    y = nowY;
+                    layoutParams.x = layoutParams.x + movedX;
+                    layoutParams.y = layoutParams.y + movedY;
+
+                    // 更新悬浮窗控件布局
+                    windowManager.updateViewLayout(view, layoutParams);
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
     }
 
 }
